a4e0456c407d8622f8d1200ed017bb9035c2682f
wirisguedes <wirisguedes@gmail.com>
2024-06-13 17:08:56 -0300

    implementacao EnderecoRepository

---
+++ src/main/java/com/iris/padroes_projeto_spring/model/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.iris.padroes_projeto_spring.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Cliente, Long>{
+
+}
