5b64442f334eadb08e64b8792bd10370cd8c676d
wirisguedes <wirisguedes@gmail.com>
2024-06-13 13:26:58 -0300

    estrutura inicial projeto

---
+++ src/main/java/com/iris/padroes_projeto_spring/PadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.iris.padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpringApplication.class, args);
+	}
+
+}
